10eafe5de49f8c650bca719abb492ff3ed9a9dbe
Irfan Ali <irfan@innostark.com>
2021-09-21 00:54:44 +0500

    linting fix

--- src/components/atom/button/index.tsx
+++ src/components/atom/button/index.tsx
@@ -1,7 +1,13 @@
 import { ButtonWrapper } from './styles';
 import { ButtonProps, RegularButtonProps } from './types';
 
-const Button = ({ variant = 'primary', size = 'normal', icon, children, ...restProps }: ButtonProps) => {
+const Button = ({
+  variant = 'primary',
+  size = 'normal',
+  icon,
+  children,
+  ...restProps
+}: ButtonProps) => {
   return (
     <ButtonWrapper {...(restProps as RegularButtonProps)} variant={variant} size={size}>
       {children}
--- src/components/atom/text-area/index.tsx
+++ src/components/atom/text-area/index.tsx
@@ -1,5 +1,7 @@
 import { TextAreaWrapper } from './styles';
 import { RegularTextAreaProps } from './types';
 
-const TextArea = (props: RegularTextAreaProps) => <TextAreaWrapper {...(props as RegularTextAreaProps)} />;
+const TextArea = (props: RegularTextAreaProps) => (
+  <TextAreaWrapper {...(props as RegularTextAreaProps)} />
+);
 export default TextArea;
